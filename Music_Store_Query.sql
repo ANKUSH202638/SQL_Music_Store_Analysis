637be7df2afca6d71a3af1e8c50c57ff3d7c76b9
ANKUSH202638 <153931783+ANKUSH202638@users.noreply.github.com>
2024-04-22 01:34:55 +0530

    Update Music_Store_Query.sql

--- Music_Store_Query.sql
+++ Music_Store_Query.sql
@@ -61,3 +61,51 @@ FROM
 GROUP BY customer.customer_id, customer.first_name , customer.last_name , customer.city , customer.country
 ORDER BY spent_money DESC
 LIMIT 1
+
+
+
+/*Question Set 2 – Moderate*/
+
+/*
+Write query to return the email, first name, last name, & Genre of all Rock Music
+listeners. Return your list ordered alphabetically by email starting with A
+*/
+
+SELECT DISTINCT
+    customer.first_name,
+    customer.Last_Name,
+    customer.email,
+    genre.name
+FROM
+    customer
+        JOIN
+    invoice ON customer.customer_id = invoice.customer_id
+        JOIN
+    invoice_line ON invoice.invoice_id = invoice_line.invoice_id
+        JOIN
+    track ON invoice_line.track_id = track.track_id
+        JOIN
+    genre ON track.genre_id = genre.genre_id
+WHERE
+    genre.name = 'Rock'
+ORDER BY customer.email
+
+
+/*Let's invite the artists who have written the most rock music in our dataset. Write a
+query that returns the Artist name and total track count of the top 10 rock bands*/
+
+SELECT 
+    artist.name, COUNT(track.album_id) AS track_count
+FROM
+    artist
+        JOIN
+    album2 ON artist.artist_id = album2.album_id
+        JOIN
+    track ON album2.album_id = track.album_id
+        JOIN
+    genre ON track.genre_id = genre.genre_id
+WHERE
+    genre.name = 'Rock'
+GROUP BY artist.name
+ORDER BY track_count DESC
+
